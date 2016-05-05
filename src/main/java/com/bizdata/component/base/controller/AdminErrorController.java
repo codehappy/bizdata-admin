edf7a69e49f9093d33a3757a102b67504b3d9ce5
343585776 <343585776@qq.com>
2016-05-05 16:54:27 +0800

    修改注释
    修改注释

--- src/main/java/com/bizdata/component/base/controller/AdminErrorController.java
+++ src/main/java/com/bizdata/component/base/controller/AdminErrorController.java
@@ -15,22 +15,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/error")
 public class AdminErrorController {
 
+	
 	/**
-	 * @author 顾剑峰<br/>
-	 *         创建时间：2015年12月17日 下午6:54:40<br/>
-	 *         描述：跳转到401未授权页面
-	 * @return
+	 * 跳转到401未授权页面
+	 *
+	 * @return String类型url值
 	 */
 	@RequestMapping(value = "/401", method = RequestMethod.GET)
 	public String error_401() {
 		return "error/401-error";
 	}
 
+	
 	/**
-	 * @author 顾剑峰<br/>
-	 *         创建时间：2015年12月17日 下午7:23:45<br/>
-	 *         描述：跳转到500系统错误页面
-	 * @return
+	 * 跳转到500系统错误页面
+	 *
+	 * @return String类型url值
 	 */
 	@RequestMapping(value = "/500", method = RequestMethod.GET)
 	public String error_500() {
